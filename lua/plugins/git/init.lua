8f95ac59e7885c0e5214051a96903aedf3cfe28a
João Inez <joaoinez.info@gmail.com>
2024-04-23 02:45:46 +0200

    Remove commented plugin

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -1,7 +1,4 @@
 return {
-  -- NOTE: Diff views
-  -- { 'sindrets/diffview.nvim', config = true },
-
   -- NOTE: Merge conflict highlighting
   { 'akinsho/git-conflict.nvim', opts = { disable_diagnostics = true }, config = true },
 
