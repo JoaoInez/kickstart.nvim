84af1ac9dc7bf3bfc80580c66b6820a406acf044
João Inez <joaoinez.info@gmail.com>
2024-05-17 08:20:52 +0200

    Add multi cursor plugin

--- lua/plugins/tools/init.lua
+++ lua/plugins/tools/init.lua
@@ -11,6 +11,18 @@ return {
   -- NOTE: Better quickfix menu
   { 'kevinhwang91/nvim-bqf', event = 'VeryLazy', opts = { func_map = { open = 'o', openc = '<CR>' } } },
 
+  -- NOTE: Multi cursors
+  --
+  -- select words with Ctrl-N
+  -- create cursors vertically with Ctrl-Down/Ctrl-Up
+  -- select one character at a time with Shift-Arrows
+  -- press n/N to get next/previous occurrence
+  -- press [/] to select next/previous cursor
+  -- press q to skip current and get next occurrence
+  -- press Q to remove current cursor/selection
+  -- start insert mode with i,a,I,A
+  { 'mg979/vim-visual-multi', branch = 'master', event = 'VeryLazy' },
+
   require 'plugins.tools.mini',
   require 'plugins.tools.trouble',
   require 'plugins.tools.vim-tmux-navigator',
