a58a3c78ab9de43a28e5849930888e6b9374fbde
João Inez <joaoinez.info@gmail.com>
2024-05-22 14:47:02 +0200

    Change flash keybindings again

--- lua/plugins/tools/flash.lua
+++ lua/plugins/tools/flash.lua
@@ -4,12 +4,58 @@ return {
   event = 'VeryLazy',
   keys = {
     {
+      'ss',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').jump {
+          search = { forward = true, wrap = false, multi_window = false },
+        }
+      end,
+      desc = 'Flash forwards',
+    },
+    {
+      'sw',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').jump {
+          search = { forward = false, wrap = false, multi_window = false },
+        }
+      end,
+      desc = 'Flash backwards',
+    },
+    {
+      'sj',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash jump',
+    },
+    {
+      'sc',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').jump {
+          pattern = vim.fn.expand '<cword>',
+        }
+      end,
+      desc = 'Flash current word',
+    },
+    {
       'sb',
       mode = { 'n', 'o', 'x' },
       function()
         require('flash').treesitter()
       end,
-      desc = 'Select block',
+      desc = 'Surround block',
+    },
+    {
+      'sB',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Surround search block',
     },
   },
   opts = {
