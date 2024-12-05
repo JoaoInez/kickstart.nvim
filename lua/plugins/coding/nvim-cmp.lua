847b5e0929b09720d65f32bd90877d049bf61ce6
João Inez <joaoinez.info@gmail.com>
2024-12-05 15:44:25 +0100

    Small nvim cmp changes

--- lua/plugins/coding/nvim-cmp.lua
+++ lua/plugins/coding/nvim-cmp.lua
@@ -20,12 +20,12 @@ return { -- Autocompletion
         -- `friendly-snippets` contains a variety of premade snippets.
         --    See the README about individual language/framework/plugin snippets:
         --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
       },
     },
     'saadparwaiz1/cmp_luasnip',
@@ -45,7 +45,9 @@ return { -- Autocompletion
     local tailwind_colorizer = require 'tailwindcss-colorizer-cmp'
 
     luasnip.config.setup {}
-    tailwind_colorizer.setup {}
+    tailwind_colorizer.setup {
+      color_square_width = 2,
+    }
 
     cmp.setup {
       formatting = {
